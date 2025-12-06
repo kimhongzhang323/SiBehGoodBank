f4a791d692317c8b3f9861f18a64f32d6515ebcb
小金少 <kim.hong.zhang323@gmail.com>
2025-12-06 12:20:11 +0800

    fix: Resolve merge conflict in screens.dart by removing unnecessary exports

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,9 +1,6 @@
 export 'onboarding_screen.dart';
 export 'home_screen.dart';
 export 'analytics_screen.dart';
-<<<<<<< HEAD
 export 'setup/setup.dart';
-=======
 export 'ai_chat_screen.dart';
 export 'root_shell.dart';
->>>>>>> 342e77be0455358c0a9919cd69962fd2ab4646c4
